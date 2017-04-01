493d99439b7c249e3adc49eeeee9e5e8ee068d51
Bryan <medzo@uab.edu>
2017-04-01 15:53:52 -0500

    Work we did on April fools day.

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,4 +1,30 @@
 
 public class Matrix {
 
+	private int rows;
+	private int columns;
+	
+	private int[][] matrix;
+	
+	public Matrix(int c, int r){
+		this.rows = r;
+		this.columns = c;
+	}
+	
+	public int getRows(){
+		return rows;
+	}
+
+	public int getColumns(){
+		return columns;
+	}
+	
+	public void setValue(int row, int col, int value){
+		matrix[row][col] = value;
+	}
+	
+	public int getValue(int row, int col){
+		return matrix[row][col];
+	}
+	
 }
